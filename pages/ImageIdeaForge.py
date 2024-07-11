262c53cd766d8af0e5c25bb900342ada08ce5424
unknown <sarthak.agrawal1311@gmail.com>
2024-07-11 16:37:19 +0530

    Refactored Code

--- pages/ImageIdeaForge.py
+++ pages/ImageIdeaForge.py
@@ -26,29 +26,29 @@ uploaded_image = st.file_uploader("Choose Image", type=['png', 'jpg', 'jpeg'])
 if uploaded_image: # Image Preview
     st.image(uploaded_image, width=256)
 
-if st.button("Generate"):
-    
-    img_b64 = image_to_base64(uploaded_image)
-    
-    llm = Ollama(model=selected_model, temperature=0.7, keep_alive=-1, num_ctx=256)
+    if st.button("Generate"):
+        
+        img_b64 = image_to_base64(uploaded_image)
+        
+        llm = Ollama(model=selected_model, temperature=0.7, keep_alive=-1, num_ctx=256)
 
-    llm_with_image_context = llm.bind(images=[img_b64])
+        llm_with_image_context = llm.bind(images=[img_b64])
 
-    system_prompt = """You are an assistant who describes the content and composition of images. Describe only what you see in the image, not what you think the image is about.Be factual and literal. Do not use metaphors or similes. Be concise, Create a image generation prompt that fits the image, don't use "", don't use imperatives like "generate, create" etc., just describe what you see in the image in a way that could be used as a prompt for that specific image. If there are words or something that looks like a copyright symbol, make no mention of it."""
+        system_prompt = """You are an assistant who describes the content and composition of images. Describe only what you see in the image, not what you think the image is about.Be factual and literal. Do not use metaphors or similes. Be concise, Create a image generation prompt that fits the image, don't use "", don't use imperatives like "generate, create" etc., just describe what you see in the image in a way that could be used as a prompt for that specific image. If there are words or something that looks like a copyright symbol, make no mention of it."""
 
-    chat_template = ChatPromptTemplate.from_messages(
-        [
-            ("system", system_prompt),
-            ("human", "{user_input}."),
+        chat_template = ChatPromptTemplate.from_messages(
+            [
+                ("system", system_prompt),
+                ("human", "{user_input}."),
 
-        ]
-    )
+            ]
+        )
 
-    str_output_parser = StrOutputParser()
+        str_output_parser = StrOutputParser()
 
-    chain = chat_template | llm_with_image_context | str_output_parser
-    result_prompt = chain.invoke({"user_input":user_input})
-    st.write(result_prompt)
+        chain = chat_template | llm_with_image_context | str_output_parser
+        result_prompt = chain.invoke({"user_input":user_input})
+        st.write(result_prompt)
     
 
 
